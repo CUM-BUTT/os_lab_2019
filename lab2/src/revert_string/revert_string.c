30f33e58623262e8f460172544b173d27a605706
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-03-08 12:48:10 +0000

    2 task added

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,14 @@
 
 void RevertString(char *str)
 {
-	// your code here
-}
+char *buff=malloc(sizeof(char)*(strlen(str)+1));
+int i=0;
 
+strcpy(buff, str);
+for(i=0;i<strlen(str);i++)
+{
+str[strlen(str)-1-i]=buff[i];
+}
+// *str=*buff;
+// your code here
+}
