dafff5b9a6efedbc6490f462f092ee89b6e67e2f
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-03-24 09:53:29 +0000

    3_task_finished

--- lab3/src/script.sh
+++ lab3/src/script.sh
@@ -4,6 +4,8 @@
 #gcc -std=c99 find_min_max.c sequential_min_max.c utils.c -o 1_task
 gcc -std=c99 find_min_max.c parallel_min_max.c utils.c -o 3_task 
 ./3_task --seed "10" --array_size "10" --pnum "10"
+./3_task --seed "10" --array_size "10" --pnum "10" --by_files "0"
+./3_task --seed "10" --array_size "10" --pnum "10" -f
 #./1_task 10 10
 
 git add ./src
