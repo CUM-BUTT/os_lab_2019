c27cb9fe697455dfc23fcc1281eba5bd86372ca0
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-04-19 09:22:00 +0000

    zombie

---
+++ lab4/src/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#cd cpp-hello-world/lab4/src
+#gcc -std=c99 find_min_max.c sequential_min_max.c utils.c -o 1_task
+gcc -std=c99 find_min_max.c parallel_min_max.c utils.c -o 3_task 
+gcc   zombie.c -o zombie
+./3_task --seed "10" --array_size "10" --pnum "10"
+./3_task --seed "10" --array_size "10" --pnum "10" --by_files "0"
+./3_task --seed "10" --array_size "10" --pnum "10" -f
+#./1_task 10 10
+
+git add ./src
+git push
+CUM-BUTT
+1809-1832i
+git commit -m 3_task
