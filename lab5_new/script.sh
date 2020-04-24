200e5ab2139c0ab59c1eeb8b537b2a645fd19900
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-04-24 15:09:17 +0000

    add lab5 new

--- lab5_new/script.sh
+++ lab5_new/script.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 git add ./
 git push
-cat "CUM-BUTT"
-cat "1809-1832i" >
-echo git commit -m "add lab5 new"
+gksu 'gnome-terminal -e mc' <<EOF
+CUM-BUTT
+EOF
+gksu 'gnome-terminal -e mc' <<EOF
+1809-1832i
+EOF
+#cat "CUM-BUTT"
+#cat "1809-1832i"  
+git commit -m "add lab5 new"
 
 #cd cpp-hello-world/lab5_new
 #chmod ugo+x script.sh
