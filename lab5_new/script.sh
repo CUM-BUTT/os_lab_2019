673c7d479ec4c3512172161b9bef2fdfecf813ee
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-04-25 09:21:35 +0000

    add lab5 new

--- lab5_new/script.sh
+++ lab5_new/script.sh
@@ -3,7 +3,7 @@ git add ./
 git push
 git commit -m "add lab5 new"
 
-#./parallel_sum --seed "10" --array_size "10" --threads_num "10"
+#./parallel_sum --seed "10" --array_size "10" --threads_num "6"
 #cd cpp-hello-world/lab5_new
 #chmod ugo+x script.sh 
 #git config credential.helper store
