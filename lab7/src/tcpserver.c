7697c85ef8bdc6d1d583d71f42eb8868f25215eb
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-06-01 08:35:08 +0000

    start lab6

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -1,62 +1,114 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+#include <arpa/inet.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <getopt.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
+#define VERBOSE
 #define SADDR struct sockaddr
+#define SLEN sizeof(struct sockaddr_in)
 
-int main() {
-  const size_t kSize = sizeof(struct sockaddr_in);
-
-  int lfd, cfd;
-  int nread;
-  char buf[BUFSIZE];
+int main(int argc, char *argv[]) {
+  int buf_size = -1;
+  int port = -1;
+  char ip[16] = {'\0'};
+  int sockfd, n;
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
-  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
-
-  memset(&servaddr, 0, kSize);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
-    perror("bind");
-    exit(1);
-  }
-
-  if (listen(lfd, 5) < 0) {
-    perror("listen");
-    exit(1);
-  }
-
   while (1) {
-    unsigned int clilen = kSize;
+    int current_optind = optind ? optind : 1;
 
-    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
-      perror("accept");
-      exit(1);
-    }
-    printf("connection established\n");
+    static struct option options[] = {{"buf_size", required_argument, 0, 0},
+                                      {"ip", required_argument, 0, 0},
+                                      {"port", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
-    }
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
 
-    if (nread == -1) {
-      perror("read");
-      exit(1);
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 0: {
+      switch (option_index) {
+      case 0:
+        if ((buf_size = atoi(optarg)) == 0) {
+          printf("Error: bad buf_size value\n");
+          return -1;
+        }
+        break;
+      case 1:
+        strcpy(ip, optarg);
+        break;
+      case 2:
+        if ((port = atoi(optarg)) == 0) {
+          printf("Error: bad port value\n");
+          return -1;
+        }
+        break;
+      default:
+        printf("Index %d is out of options\n", option_index);
+      }
+    } break;
+
+    case '?':
+      printf("Arguments error\n");
+      break;
+    default:
+      fprintf(stderr, "getopt returned character code 0%o?\n", c);
     }
-    close(cfd);
   }
+
+  if (buf_size == -1 || port == -1 || strlen(ip) == 0) {
+    fprintf(stderr, "Using: %s --buf_size [NUMBER] --port [NUMBER]\n",
+            argv[0]);
+    return -1;
+  }
+  char sendline[buf_size], recvline[buf_size + 1];
+
+#ifdef VERBOSE
+  printf("buf=%d port=%d ip=%s\n", buf_size, port, ip);
+#endif
+
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(port);
+
+  if (inet_pton(AF_INET, ip, &servaddr.sin_addr) < 0) {
+    perror("inet_pton problem");
+    exit(1);
+  }
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket problem");
+    exit(1);
+  }
+
+  write(1, "Enter string\n", 13);
+
+  while ((n = read(0, sendline, buf_size)) > 0) {
+    if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
+      perror("sendto problem");
+      exit(1);
+    }
+
+    /* Get echo */
+    /* Do not remember sender (NULL, NULL) - no need */
+    memset(recvline, 0, sizeof(recvline));
+    if (recvfrom(sockfd, recvline, buf_size, 0, NULL, NULL) == -1) 
+    {
+      perror("recvfrom problem");
+      exit(1);
+    }
+
+    printf("REPLY FROM SERVER= %s\n", recvline);
+  }
+  close(sockfd);
 }
