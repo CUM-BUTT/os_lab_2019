fcafde550d700ae421b260d20ff235851688dd48
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-06-08 22:33:43 +0000

    start lab6

--- lab7/src1/script.sh
+++ lab7/src1/script.sh
@@ -5,8 +5,10 @@ git add ./
 git push
 git commit -m "start lab6"
 
-#cd cpp-hello-world/lab7/src
+#cd cpp-hello-world/lab7/src1
 #./parallel_fac.out --k "21" --pnum "3" --mod "6"
 #chmod ugo+x script.sh 
 #chmod ugo+x run.sh 
 #git config credential.helper store
+#./udpclient --port 20001 --bufsize 4 --ip 127.0.0.1
+#./udpserver --port 20001 --bufsize 4
