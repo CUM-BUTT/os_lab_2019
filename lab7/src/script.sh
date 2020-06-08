77f83f14bc76f5c2e032dcdc4af12fcfad8d0217
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-06-08 11:31:43 +0000

    start lab6

--- lab7/src/script.sh
+++ lab7/src/script.sh
@@ -4,12 +4,8 @@ git config credential.helper store
 git add ./
 git push
 git commit -m "start lab6"
-make
 
 #cd cpp-hello-world/lab7/src
 #./parallel_fac.out --k "21" --pnum "3" --mod "6"
 #chmod ugo+x script.sh 
 #git config credential.helper store
-
-#./tcpclient.out --buf_size 10 --port 1488 --ip 93.80.179.190
-#./tcpserver.out --buf_size 10 --port 1488 --ip 93.80.179.190
