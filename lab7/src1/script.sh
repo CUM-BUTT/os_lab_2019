19ebce429afe9aadb9610f95956f0a6a32066d2c
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-06-08 22:25:57 +0000

    start lab6

---
+++ lab7/src1/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config credential.helper store
+git add ./
+git push
+git commit -m "start lab6"
+
+#cd cpp-hello-world/lab7/src
+#./parallel_fac.out --k "21" --pnum "3" --mod "6"
+#chmod ugo+x script.sh 
+#chmod ugo+x run.sh 
+#git config credential.helper store
