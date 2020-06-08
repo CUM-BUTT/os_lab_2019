19330279a6d6b049c2fafa49881b00bf90cd2ebe
cum_butt <ilya-degtyarev-1998@mail.ru>
2020-06-08 13:13:32 +0000

    start lab6

--- lab7/src/run.sh
+++ lab7/src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./tcpserver.out --buf_size 3 --port 3000 --ip 127.0.0.1
-./tcpclient.out --buf_size 3 --port 1000 --ip 153.0.0.1
+./tcpserver.out --buf_size 3 --port 3001 --ip 127.0.0.1
+./tcpclient.out --buf_size 3 --port 3000 --ip 127.0.0.1
